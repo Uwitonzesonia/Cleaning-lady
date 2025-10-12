fed9189f7812f3ba5d275bbbcd003e0d359b544d
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-10-12 07:42:54 -0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
-//import HowItWorks from "./components/HowItWorks";
-//import Pricing from "./components/Pricing";
-//import Testimonials from "./components/Testimonials";
 import Contacts from "./components/Contacts";
 import Footer from "./components/Footer";
 
@@ -15,7 +12,7 @@ function App() {
       <main>
         <Hero />
         <Services />
-        <Contacts /> 
+        <Contacts />
       </main>
       <Footer />
     </>
